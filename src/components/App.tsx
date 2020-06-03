d1f0121be2bbeda30a8044d749fff12d0dd215d2
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-06-03 14:06:40 +0200

    add styled-components, add Global Style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import Pages from './pages/Pages';
 
 export interface AppProps {}
@@ -7,7 +8,25 @@ export interface AppProps {}
 const App: React.FC<AppProps> = () => (
   <Router basename={process.env.PUBLIC_URL}>
     <Pages />
+    <GlobalStyle />
   </Router>
 );
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+}
+body {
+  font-family: Roboto, Arial, sans-serif;
+  background-color: #181818;
+}
+`;
