d93ef347782613eb4174de121ef3bbcf16463d79
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-06-04 20:06:16 +0200

    add CompanyView route

--- src/components/pages/Pages.tsx
+++ src/components/pages/Pages.tsx
@@ -2,12 +2,14 @@ import React, { Suspense } from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 
 const Companies = React.lazy(() => import('./companies/Companies'));
+const CompanyView = React.lazy(() => import('./companyView/CompanyView'));
 
 const Pages: React.FC = () => (
   <Suspense fallback={<div>Loading...</div>}>
     <Switch>
       <Redirect exact from="/" to="/companies" />
       <Route exact path="/companies" component={Companies} />
+      <Route exact path="/companies/:id" component={CompanyView} />
     </Switch>
   </Suspense>
 );
