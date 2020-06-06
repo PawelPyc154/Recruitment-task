eee797ee758fcdf375872c562f9310c0cb301025
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-06-06 18:19:24 +0200

    add loader suspense style

--- src/components/pages/Pages.tsx
+++ src/components/pages/Pages.tsx
@@ -1,12 +1,12 @@
 import React, { Suspense } from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
-import Loader from '../Loader';
+import Loader from './Loader';
 
 const Companies = React.lazy(() => import('./companies/Companies'));
 const CompanyView = React.lazy(() => import('./companyView/CompanyView'));
 
 const Pages: React.FC = () => (
-  <Suspense fallback={<Loader />}>
+  <Suspense fallback={<Loader height="100vh" />}>
     <Switch>
       <Redirect exact from="/" to="/companies" />
       <Route exact path="/companies" component={Companies} />
