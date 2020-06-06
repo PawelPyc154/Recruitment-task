5f18472b45cbdc64bc5f4afdb7bb33099311210b
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-06-06 18:18:30 +0200

    replace loader

--- src/components/pages/Loader.tsx
+++ src/components/pages/Loader.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import { CircularProgress } from '@material-ui/core';
 
-export interface LoaderProps {}
+export interface LoaderProps {
+  height?: string;
+}
 
-const Loader: React.FC<LoaderProps> = () => (
-  <Container>
+const Loader: React.FC<LoaderProps> = ({ height }) => (
+  <Container height={height}>
     <CircularProgressStyled />
   </Container>
 );
 export default Loader;
 
-const Container = styled.div`
+const Container = styled.div<LoaderProps>`
   width: 100%;
-  height: 100%;
+  height: ${({ height }) => height || '100%'};
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: auto;
-  position: absolute;
 `;
 const CircularProgressStyled = styled(CircularProgress)`
   &.MuiCircularProgress-colorPrimary {
