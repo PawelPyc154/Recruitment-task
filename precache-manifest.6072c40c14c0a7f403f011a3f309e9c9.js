f4619782ee510fea681dd1a70ffbe41a9f411b24
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-06-08 15:38:20 +0200

    Updates

---
+++ precache-manifest.6072c40c14c0a7f403f011a3f309e9c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556bbf0fbbfb79075e1ff1bbfb177f5e",
+    "url": "/Recruitment-task/index.html"
+  },
+  {
+    "revision": "6ba880bab867abd92e53",
+    "url": "/Recruitment-task/static/js/0.082f099b.chunk.js"
+  },
+  {
+    "revision": "b3ddbc680a3ac598c3b0",
+    "url": "/Recruitment-task/static/js/3.217e61e4.chunk.js"
+  },
+  {
+    "revision": "dd3eb7fd2c17ef33c48986b75a628ddc",
+    "url": "/Recruitment-task/static/js/3.217e61e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c5c65930addf8a306b",
+    "url": "/Recruitment-task/static/js/4.caf7c8c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Recruitment-task/static/js/4.caf7c8c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f831fa10c5f52861091",
+    "url": "/Recruitment-task/static/js/5.59c62bea.chunk.js"
+  },
+  {
+    "revision": "c7ad6fbc95cfdccd8012",
+    "url": "/Recruitment-task/static/js/6.8fcf9339.chunk.js"
+  },
+  {
+    "revision": "31dd4eb5cc90046a1dce",
+    "url": "/Recruitment-task/static/js/7.a29e7f52.chunk.js"
+  },
+  {
+    "revision": "67fc046146b20ddd16f5",
+    "url": "/Recruitment-task/static/js/main.a7bbaf10.chunk.js"
+  },
+  {
+    "revision": "5cc97c15f1ab39d67487",
+    "url": "/Recruitment-task/static/js/runtime-main.1b074661.js"
+  }
+]);
